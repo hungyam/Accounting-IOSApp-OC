176674c5b400f811620053936c41c9d3fe4498ad
123 <123@1.x>
2021-12-27 07:18:45 -0800

    6

--- finalhw/PersonalPage/SetingSrc/ChangePassword.m
+++ finalhw/PersonalPage/SetingSrc/ChangePassword.m
@@ -7,18 +7,21 @@
 
 #import <Foundation/Foundation.h>
 #import "ChangePassword.h"
+#import "PersonalVC.h"
 
 #define BackColor [UIColor colorWithRed:242/255.0 green:242/255.0 blue:247/255.0 alpha:1]
 #define MainColor [UIColor colorWithRed:244/255.0 green:92/255.0 blue:99/255.0 alpha:1]
 #define Wper *self.view.bounds.size.width/100
 #define Hper *self.view.bounds.size.height/100
 
-@interface ChangePassword ()
+@interface ChangePassword (){
+    NSString *password;
+}
 
 @property (nonatomic, strong) UILabel *passwordLabel;
 @property (nonatomic, strong) UITextView *passwordText;
-@property (nonatomic, strong) UILabel *rePasswordLabel;
-@property (nonatomic, strong) UITextView *rePasswordText;
+@property (nonatomic, strong) UILabel *newPasswordLabel;
+@property (nonatomic, strong) UITextView *newPasswordText;
 
 @end
 
@@ -29,12 +32,22 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = BackColor;
     self.navigationController.navigationBar.tintColor = MainColor;
+    [self.view addSubview:self.passwordLabel];
+    [self.view addSubview:self.passwordText];
+    [self.view addSubview:self.newPasswordLabel];
+    [self.view addSubview:self.newPasswordText];
+}
+
+- (void)loadUserMes {
+    PersonalMes *mes = [DataManage getPersonalMes];
+    password = mes.password;
 }
 
 - (UILabel *)passwordLabel {
     if (_passwordLabel == nil) {
-        _passwordLabel = [[UILabel alloc]initWithFrame:CGRectMake(8 Wper, 59 Hper, 150, 30)];
-        _passwordLabel.text = @"密码";
+        _passwordLabel = [[UILabel alloc]initWithFrame:CGRectMake(50 Wper-75, 9 Hper, 150, 30)];
+        _passwordLabel.text = @"原密码";
+        _passwordLabel.textAlignment = NSTextAlignmentCenter;
         _passwordLabel.textColor = MainColor;
     }
     return _passwordLabel;
@@ -42,36 +55,39 @@
 
 - (UITextView *)passwordText {
     if (_passwordText == nil) {
-        _passwordText = [[UITextView alloc]initWithFrame:CGRectMake(8 Wper, 64 Hper - 15, 80 Wper, 40)];
+        _passwordText = [[UITextView alloc]initWithFrame:CGRectMake(10 Wper, 17 Hper - 15, 80 Wper, 40)];
         _passwordText.font = [UIFont systemFontOfSize:23 weight:UIFontWeightMedium];
         CALayer *layer = [CALayer layer];
         layer.frame = CGRectMake(0, _passwordText.frame.size.height - 1, _passwordText.frame.size.width, 1);
-        layer.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.05].CGColor;
+        layer.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1].CGColor;
+        _passwordText.backgroundColor = [UIColor clearColor];
         [_passwordText.layer addSublayer:layer];
         [_passwordText setScrollEnabled:NO];
     }
     return _passwordText;
 }
 
-- (UILabel *)rePasswordLabel {
-    if (_rePasswordLabel == nil) {
-        _rePasswordLabel = [[UILabel alloc]initWithFrame:CGRectMake(8 Wper, 67 Hper, 150, 30)];
-        _rePasswordLabel.text = @"确认密码";
-        _rePasswordLabel.textColor = MainColor;
+- (UILabel *)newPasswordLabel {
+    if (_newPasswordLabel == nil) {
+        _newPasswordLabel = [[UILabel alloc]initWithFrame:CGRectMake(50 Wper-75, 25 Hper, 150, 30)];
+        _newPasswordLabel.text = @"新密码";
+        _newPasswordLabel.textColor = MainColor;
+        _newPasswordLabel.textAlignment = NSTextAlignmentCenter;
     }
-    return _rePasswordLabel;
+    return _newPasswordLabel;
 }
 
-- (UITextView *)rePasswordText {
-    if (_rePasswordText == nil) {
-        _rePasswordText = [[UITextView alloc]initWithFrame:CGRectMake(8 Wper, 72 Hper - 15, 80 Wper, 40)];
-        _rePasswordText.font = [UIFont systemFontOfSize:23 weight:UIFontWeightMedium];
+- (UITextView *)newPasswordText {
+    if (_newPasswordText == nil) {
+        _newPasswordText = [[UITextView alloc]initWithFrame:CGRectMake(10 Wper, 32 Hper - 15, 80 Wper, 40)];
+        _newPasswordText.font = [UIFont systemFontOfSize:23 weight:UIFontWeightMedium];
         CALayer *layer = [CALayer layer];
-        layer.frame = CGRectMake(0, _rePasswordText.frame.size.height - 1, _rePasswordText.frame.size.width, 1);
-        layer.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.05].CGColor;
-        [_rePasswordText.layer addSublayer:layer];
-        [_rePasswordText setScrollEnabled:NO];
+        layer.frame = CGRectMake(0, _newPasswordText.frame.size.height - 1, _newPasswordText.frame.size.width, 1);
+        layer.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1].CGColor;
+        _newPasswordText.backgroundColor = [UIColor clearColor];
+        [_newPasswordText.layer addSublayer:layer];
+        [_newPasswordText setScrollEnabled:NO];
     }
-    return _rePasswordText;
+    return _newPasswordText;
 }
 @end
